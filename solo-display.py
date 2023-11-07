033438cc6d75c6dc3647a70708820a197dd490ed
sampob <sampo.bredenberg@keemail.me>
2023-11-07 19:28:59 +0200

    Commented and moved topics to variables.

--- solo-display.py
+++ solo-display.py
@@ -11,6 +11,10 @@ BOX_ID = os.getenv("BOX_ID")
 MQTT_BROKER_HOST = os.getenv("MQTT_BROKER_HOST")
 MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT"))
 
+SPEED_TOPIC = f"{BOX_ID}/+/speed/out/" # Topic for the speed graph
+PRESSURE_TOPIC = f"{BOX_ID}/+/pressure/out/" # Topic for the pressure graph
+FLOW_TOPIC = f"{BOX_ID}/+/flow/out/" # Topic for flow numbers
+
 plt.rcParams['toolbar'] = 'None'
 
 # Motor's speed values are stored in this list
@@ -48,9 +52,12 @@ def pressureMessage(client, userdata, message):
 Creates a matplotlib animation of line graphs and values. Animation function passes one argument. 
 """
 def animateGraphs(i):
+    # Speed graph
+    # Clear and build the graph
     ax1.clear()
     ax1.plot(range(len(speedList)), speedList, marker="")
-
+    
+    # Graph configurations
     ax1.set_ylim(1250, 2049)
     ax1.set_xlim(0, pressureSize + (pressureSize / 4))
     ax1.set_xlabel("Time")
@@ -58,23 +65,25 @@ def animateGraphs(i):
     ax1.set_xticks([])
     ax1.set_title("Speed", loc="left")
 
+    # Pressure graph
+    # Clear and build the graph
     ax2.clear()
     ax2.plot(range(len(pressureList)), pressureList, marker="")
 
-    ax2.set_ylim(0, 9999)
+    # Graph configurations
+    ax2.set_ylim(0, 9999) # Min and max values shown on the graph
     ax2.set_xlim(0, pressureSize + (pressureSize / 4))
     ax2.set_xlabel("Time")
     ax2.set_ylabel("Value")
     ax2.set_xticks([])
     ax2.set_title("Pressure", loc="left")
 
+    # Flow numbers
     ax3.clear()
     ax3.text(0, 1, f"Flow in: {flowInValue}", fontsize=24)
     ax3.text(0, 0.5, f"Flow out: {flowInValue}", fontsize=24)
     ax3.set_xticks([])
     ax3.set_yticks([])
-
-
 
 """
 When figure is closed, gracefully disconnects MQTT
@@ -89,18 +98,15 @@ if __name__ == "__main__":
 
     client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
 
-    # Subscribe to the box's topic and start receiving data
-    MOTOR_TOPIC = BOX_ID + "/motor/speed/out/"
-    
+    # Subscribe to the box's topic and bind different topics to functions
     client.subscribe([(f"{BOX_ID}/+/+/+/", 1)])
-    client.message_callback_add(MOTOR_TOPIC, speedMessage)
-    client.message_callback_add(f"{BOX_ID}/+/flow/out/", flowMessage)
-    client.message_callback_add(f"{BOX_ID}/+/pressure/out/", pressureMessage)
+    client.message_callback_add(SPEED_TOPIC, speedMessage)
+    client.message_callback_add(FLOW_TOPIC, flowMessage)
+    client.message_callback_add(PRESSURE_TOPIC, pressureMessage)
 
     client.loop_start()
 
     # Matplotlib initialization
-    # fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     fig = plt.figure()
     ax1 = fig.add_subplot(2, 1, 1)
     ax2 = fig.add_subplot(2, 2, 3)
