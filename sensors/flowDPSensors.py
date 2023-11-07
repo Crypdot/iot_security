5f02fd575ffd477d04d3ee448c030dbb440c90fe
Alex <alexandersanmig@gmail.com>
2023-11-08 01:12:09 +0200

    Added the parsing of arguments. TODO :: Operating modes for ease of use? - Alex

--- sensors/flowDPSensors.py
+++ sensors/flowDPSensors.py
@@ -1,9 +1,10 @@
 import serial
+import sys
 import time
 import os
+import argparse
 from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
-import time
 
 load_dotenv()
 
@@ -16,11 +17,36 @@ MQTT_TOPIC_SENSOR_TEMP_OUT =os.getenv("MQTT_TOPIC_SENSOR_TEMP_OUT")
 SAMPLE_RATE = float(os.getenv("MQTT_SENSOR_SAMPLERATE"))
 
 # These likely should be parameters given when the program is run. Otherwise maybe assume a default value?
-boxID = os.getenv("BOX_ID")
-sensorID = "diffPressure01"
+#boxID = os.getenv("BOX_ID")
+#sensorID = "diffPressure01"
 
-flowChannel = boxID+"/"+sensorID+MQTT_TOPIC_SENSOR_FLOW_OUT
-dpChannel = boxID+"/"+sensorID
+parser = argparse.ArgumentParser(description="A script to be run for the sensors. This code is configured to be able to run the flowrate sensors and differential pressure sensors. Use as indicated.")
+parser.add_argument("-si", type=str, required=True, help="The sensor's ID. The channel this sensor posts on will depend on this name. Name this based on the sensor; fe: 'flowrateSensor01'")
+parser.add_argument("-bi", type=str, required=True, help="The box's ID. The channel this sensor posts on will depend on this name. Name this based on the box; fe: 'box01'")
+parser.add_argument("-cp", type=str, required=True, help="The USB port. Check which port this sensor is attached to, so that it knows where to get the right data from; fe: '/dev/ttyUSB0'. Note that it may be 'ACM0' as well. Check using 'dmesg -w' before use.")
+#parser.add_argument("-i", type=int, default=1, help="Something very helpful regarding integers, for sure.")
+
+"""
+Configures the sensor to run as indicated by the arguments given.
+
+TODO :: Possibly adding an additional argument to toggle between flow- and DP-modes, so that the user can simply indicate the type of sensor on runtime?
+"""
+def setup():
+   global boxID, sensorID, flowChannel, dpChannel, sensorSerialPort
+   
+   args = parser.parse_args()
+   boxID = args.bi
+   sensorID = args.si
+   sensorPort = args.cp
+   flowChannel = boxID+"/"+sensorID+MQTT_TOPIC_SENSOR_FLOW_OUT
+   dpChannel = boxID+"/"+sensorID
+
+   """
+   Configures the serial port to listen to the arduino. 
+   NOTE :: This *should* be "USB0", but it may be "USB1" or "ACM0"; check the port. On Windows, "COM" ports are also possible.
+   """
+   print("\nConfiguring Serial Ports")
+   sensorSerialPort = sensorPort
 
 # Process Messages
 def onConnect(client, metadata, flags, rc):
@@ -54,16 +80,21 @@ def publishDP(client, channel, value):
 
    try:
       data = value.decode("UTF-8").split(",")
-      pressure = str(data[0]).strip()
-      temp = str(data[1]).strip()
       
-      print(f"Publishing '{pressure}' to '{dpChannel}'")
-      client.publish(dpChannel+"pressure", pressure)
-      print(f"Publishing '{temp}' to '{tempChannel}'")
-      client.publish(tempChannel, temp)
+      if len(data) >= 2:
+         pressure = str(data[0]).strip()
+         temp = str(data[1]).strip()
+         
+         print(f"Publishing '{pressure}' to '{dpChannel}'")
+         client.publish(dpChannel+"pressure", pressure)
+         print(f"Publishing '{temp}' to '{tempChannel}'")
+         client.publish(tempChannel, temp)
    except Exception as error:
       print(f"Something went wrong :: {error}")
 
+# Run starts here
+
+setup()
 
 print("Ventilator Test Code") 
 
@@ -71,28 +102,18 @@ print("Flow rate in SLM (standard litre per minute)\n"+
 "Differential pressure in Pascals,  1atm = 101Pa. 1cmH20 = 98 Pa, typical CPAP range 4-20\n"+
 "Temperature in Celcius\n")
 
-print(f"Using MQTT broker at {MQTT_BROKER_HOST} at {str(MQTT_BROKER_PORT)}")
-
 client = mqtt.Client()
 client.on_connect = onConnect
 client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, keepalive=60, bind_address="")
 client.loop_start()
 
-"""
-Configures the serial port to listen to the arduino. 
-This *should* be "USB0", but it may be "USB1" or "ACM0"; check the port. On Windows, "COM" ports are also possible.
-
-TODO :: Should this be a parameter given to the program on run?
-"""
-print("\nConfiguring Serial Ports")
-sensorSerialPort = "/dev/ttyUSB0"
 
 print("Connecting to sensor")
 sensor = serial.Serial(sensorSerialPort,115200,timeout=1)
 print(f"Serial connected \nPort :: {sensorSerialPort}, sensor :: {sensor}")
 
 # Give it a few seconds to set itself up. No-wait results in consistent failures, as does a single second. 
-time.sleep(5)
+time.sleep(2)
 
 sensor.flushInput()
 sensor.flushOutput()
